ca5d4b8e608fcca743850d164feff0e4e2db6f02
kzh <1432245553@qq.com>
2021-10-29 09:42:02 +0800

    1.在保证兼容性的情况下，使函数名更具可读性。统一最长公共子序列系列函数为lcs_sequence_xx()，最长公共子串系列函数为lcs_string_xx()。同时保留原有的lcs和lcs2的接口。 2.加入函数lcs_sequence_idx()，lcs_string_idx()返回子串或子序列的下标，反映两段文本的对齐关系。

--- src/main.cpp
+++ src/main.cpp
@@ -28,7 +28,7 @@ vector<string> utf8_split(const string &str){
 
 
 // 最长公共子序列（不连续）
-int lcs_length_(const string &str1, const string &str2) {
+int lcs_sequence_length(const string &str1, const string &str2) {
     if (str1 == "" || str2 == "")
         return 0;
     vector<string> s1 = utf8_split(str1);
@@ -61,8 +61,51 @@ int lcs_length_(const string &str1, const string &str2) {
 }
 
 
+vector<int> lcs_sequence_idx(const string &str, const string &ref) {
+    vector<string> s1 = utf8_split(str);
+    vector<string> s2 = utf8_split(ref);
+    int m = s1.size();
+    int n = s2.size();
+    vector<vector<int>> dp(m + 1, vector<int>(n + 1));
+    vector<vector<char>> direct(m + 1, vector<char>(n + 1));
+    vector<int> res(m, -1);
+    if (m == 0 || n == 0)
+        return res;
+
+    int i, j;
+    for (i = 0; i <= m; i++) dp[i][0] = 0;
+    for (j = 0; j <= n; j++) dp[0][j] = 0;
+    for (i = 1; i <= m; i++) {
+        for (j = 1; j <= n; j++) {
+            if (s1[i - 1] == s2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+                direct[i][j] = 'm';     // match
+            } else {
+                if (dp[i - 1][j] >= dp[i][j - 1]) {
+                    dp[i][j] = dp[i - 1][j];
+                    direct[i][j] = 's';     // str+1
+                }
+                else {
+                    dp[i][j] = dp[i][j-1];
+                    direct[i][j] = 'r';     // ref+1
+                }
+            }
+        }
+    }
+    for (i = m, j = n; i > 0 && j > 0; ){
+        if (direct[i][j] == 'm') {
+            res[i-1] = j-1;
+            i--; j--;
+        }
+        else if (direct[i][j] == 's') i--;
+        else if (direct[i][j] == 'r') j--;
+    }
+    return res;
+}
+
+
 // 最长公共子串（连续）
-int lcs2_length_(const string &str1, const string &str2) {
+int lcs_string_length(const string &str1, const string &str2) {
     if (str1 == "" || str2 == "")
         return 0;
     vector<string> s1 = utf8_split(str1);
@@ -96,34 +139,60 @@ int lcs2_length_(const string &str1, const string &str2) {
 }
 
 
-// TODO 返回子序列
-int lcs(const string &str1, const string &str2){
-    return lcs_length_(str1, str2);
+vector<int> lcs_string_idx(const string &str, const string &ref) {
+    vector<string> s1 = utf8_split(str);
+    vector<string> s2 = utf8_split(ref);
+    int m = s1.size();
+    int n = s2.size();
+    vector<vector<int>> dp(m + 1, vector<int>(n + 1));
+    vector<int> res(m, -1);
+    if (m == 0 || n == 0)
+        return res;
+
+    int i, j;
+    int max_i = 0, max_j = 0;
+    for (i = 0; i <= m; i++) {
+        dp[i][0] = 0;
+    }
+    for (j = 0; j <= n; j++) {
+        dp[0][j] = 0;
+    }
+    for (i = 1; i <= m; i++) {
+        for (j = 1; j <= n; j++) {
+            if (s1[i - 1] == s2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+                if (dp[i][j] > dp[max_i][max_j]){
+                    max_i = i; max_j = j;
+                }
+            }
+            else {
+                dp[i][j] = 0;
+            }
+        }
+    }
+    for (i = 0; i < dp[max_i][max_j]; i++) {
+        res[max_i-i-1] = max_j-i-1;
+    }
+    return res;
 }
 
 
-// TODO 返回子串
-int lcs2(const string &str1, const string &str2){
-    return lcs2_length_(str1, str2);
-}
-
-
-vector<int> lcs_of_list(const string &str1, vector<string> &str_list){
+vector<int> lcs_sequence_of_list(const string &str1, vector<string> &str_list){
     int size = str_list.size();
     vector<int> ls(size);
     for (int i = 0; i < size; i++){
-        int l = lcs(str1, str_list[i]);
+        int l = lcs_sequence_length(str1, str_list[i]);
         ls[i] = l;
     }
     return ls;
 }
 
 
-vector<int> lcs2_of_list(const string &str1, vector<string> &str_list){
+vector<int> lcs_string_of_list(const string &str1, vector<string> &str_list){
     int size = str_list.size();
     vector<int> ls(size);
     for (int i = 0; i < size; i++){
-        int l = lcs2(str1, str_list[i]);
+        int l = lcs_string_length(str1, str_list[i]);
         ls[i] = l;
     }
     return ls;
@@ -175,35 +244,20 @@ namespace py = pybind11;
 
 
 PYBIND11_MODULE(pylcs, m) {
-    m.def("lcs", &lcs, R"pbdoc(
-        Longest common subsequence
-    )pbdoc");
+    m.def("lcs", &lcs_sequence_length, R"pbdoc(Longest common subsequence)pbdoc");
+    m.def("lcs_sequence_length", &lcs_sequence_length, R"pbdoc(Longest common subsequence)pbdoc");
+    m.def("lcs_sequence_idx", &lcs_sequence_idx, R"pbdoc(Longest common subsequence indices mapping from str to ref)pbdoc");
+    m.def("lcs_of_list", &lcs_sequence_of_list, R"pbdoc(Longest common subsequence of list)pbdoc");
+    m.def("lcs_sequence_of_list", &lcs_sequence_of_list, R"pbdoc(Longest common subsequence of list)pbdoc");
 
-    m.def("lcs_of_list", &lcs_of_list, R"pbdoc(
-        Longest common subsequence of list
-    )pbdoc");
+    m.def("lcs2", &lcs_string_length, R"pbdoc(Longest common substring)pbdoc");
+    m.def("lcs_string_length", &lcs_string_length, R"pbdoc(Longest common substring)pbdoc");
+    m.def("lcs_string_idx", &lcs_string_idx, R"pbdoc(Longest common substring indices mapping from str to ref)pbdoc");
+    m.def("lcs2_of_list", &lcs_string_of_list, R"pbdoc(Longest common substring of list)pbdoc");
+    m.def("lcs_string_of_list", &lcs_string_of_list, R"pbdoc(Longest common substring of list)pbdoc");
 
-    m.def("lcs2", &lcs2, R"pbdoc(
-        Longest common substring
-    )pbdoc");
-
-    m.def("lcs2_of_list", &lcs2_of_list, R"pbdoc(
-        Longest common substring of list
-    )pbdoc");
-
-    m.def("levenshtein_distance", &levenshtein_distance, R"pbdoc(
-        Levenshtein Distance of Two Strings
-    )pbdoc");
-
-    m.def("edit_distance", &levenshtein_distance, R"pbdoc(
-        Same As levenshtein_distance(): Levenshtein Distance of Two Strings
-    )pbdoc");
-
-    m.def("levenshtein_distance_of_list", &levenshtein_distance_of_list, R"pbdoc(
-        Levenshtein Distance of one string to a list of strings
-    )pbdoc");
-
-    m.def("edit_distance_of_list", &levenshtein_distance_of_list, R"pbdoc(
-        Levenshtein Distance of one string to a list of strings
-    )pbdoc");
+    m.def("levenshtein_distance", &levenshtein_distance, R"pbdoc(Levenshtein Distance of Two Strings)pbdoc");
+    m.def("edit_distance", &levenshtein_distance, R"pbdoc(Same As levenshtein_distance(): Levenshtein Distance of Two Strings)pbdoc");
+    m.def("levenshtein_distance_of_list", &levenshtein_distance_of_list, R"pbdoc(Levenshtein Distance of one string to a list of strings)pbdoc");
+    m.def("edit_distance_of_list", &levenshtein_distance_of_list, R"pbdoc(Levenshtein Distance of one string to a list of strings)pbdoc");
 }
--- tests/test_pylcs.py
+++ tests/test_pylcs.py
@@ -3,27 +3,55 @@ import pylcs
 
 
 def test_lcs():
+    assert pylcs.lcs("aaa", "") == 0
+    assert pylcs.lcs("", "bbb") == 0
     assert pylcs.lcs("aaa", "bbb") == 0
-    assert pylcs.lcs("aaa", "aabbbaa") == 3
     assert pylcs.lcs("你好", "中国") == 0
-    assert pylcs.lcs("aaa你好", "你好呀") == 2
+    assert pylcs.lcs_sequence_length("aaa", "aabbbaa") == 3
+    assert pylcs.lcs_sequence_length("aaa你好", "你好呀") == 2
 
 
 def test_lcs_of_list():
     assert pylcs.lcs_of_list("aaa", ["aabbbaa"] * 10) == [3] * 10
     assert pylcs.lcs_of_list("aaa你好", ["你好呀"] * 10) == [2] * 10
+    assert pylcs.lcs_sequence_of_list("aaa", ["aabbbaa"] * 10) == [3] * 10
+    assert pylcs.lcs_sequence_of_list("aaa你好", ["你好呀"] * 10) == [2] * 10
+
+
+def test_lcs_sequence_idx():
+    assert pylcs.lcs_sequence_idx("", "bbb") == []
+    assert pylcs.lcs_sequence_idx("aaa", "") == [-1, -1, -1]
+    assert pylcs.lcs_sequence_idx("aaa", "bbb") == [-1, -1, -1]
+    assert pylcs.lcs_sequence_idx("你好", "中国") == [-1, -1]
+    res = pylcs.lcs_sequence_idx("aaa", "aabbbaa")
+    assert all([x in [0, 1, 5, 6] for x in res]) and res == sorted(res)
+    assert pylcs.lcs_sequence_idx("aaa你好", "你好呀") == [-1, -1, -1, 0, 1]
 
 
 def test_lcs2():
+    assert pylcs.lcs2("aaa", "") == 0
+    assert pylcs.lcs2("", "bbb") == 0
     assert pylcs.lcs2("aaa", "bbb") == 0
-    assert pylcs.lcs2("aaa", "aabbbaa") == 2
     assert pylcs.lcs2("你好", "中国") == 0
-    assert pylcs.lcs2("aaa你好", "好呀你") == 1
+    assert pylcs.lcs_string_length("aaa", "aabbbaa") == 2
+    assert pylcs.lcs_string_length("aaa你好", "好呀你") == 1
 
 
 def test_lcs2_of_list():
     assert pylcs.lcs2_of_list("aaa", ["aabbbaa"] * 10) == [2] * 10
     assert pylcs.lcs2_of_list("aaa你好", ["好呀你"] * 10) == [1] * 10
+    assert pylcs.lcs_string_of_list("aaa", ["aabbbaa"] * 10) == [2] * 10
+    assert pylcs.lcs_string_of_list("aaa你好", ["好呀你"] * 10) == [1] * 10
+
+
+def test_lcs_string_idx():
+    assert pylcs.lcs_string_idx("", "bbb") == []
+    assert pylcs.lcs_string_idx("aaa", "") == [-1, -1, -1]
+    assert pylcs.lcs_string_idx("aaa", "bbb") == [-1, -1, -1]
+    assert pylcs.lcs_string_idx("你好", "中国") == [-1, -1]
+    assert pylcs.lcs_string_idx("aaa", "aabbbaa") in ([0, 1, -1], [-1, 0, 1], [5, 6, -1], [-1, 5, 6])
+    assert pylcs.lcs_string_idx("aaa", "aabbbaaa") == [5, 6, 7]
+    assert pylcs.lcs_string_idx("aaa你好", "你好呀") == [-1, -1, -1, 0, 1]
 
 
 def test_edit_distance():
@@ -37,3 +65,14 @@ def test_edit_distance_of_list():
     assert pylcs.edit_distance_of_list("aaa", ["bbb"] * 10) == [3] * 10
     assert pylcs.edit_distance_of_list("aaa你好", ["你好呀"] * 10) == [4] * 10
     assert pylcs.edit_distance_of_list("aaa你好", ["bbb", "你好呀"] * 10) == [5, 4] * 10
+
+
+test_lcs()
+test_lcs_of_list()
+test_lcs_sequence_idx()
+test_lcs_string_idx()
+test_lcs2()
+test_lcs2()
+test_lcs2_of_list()
+test_edit_distance()
+test_edit_distance_of_list()
